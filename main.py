5b95ecbeb4c7a020fa4d8d70780f5b26098429c7
Zui-sou <edanrohner@gmail.com>
2025-02-23 16:16:54 -0700

    dev branch

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-import base64
 import json
-import os
 
-from dotenv import load_dotenv
 from requests import get, post
 
-load_dotenv()
-
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
+from auth import get_auth_header, get_token
 
 
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
-
-
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
 
 
 def search_for_artist(token, artist_name):
@@ -54,36 +29,13 @@ def get_songs(token, artist_id):
     json_result = json.loads(result.content)["tracks"]
     return json_result
 
-def find_playlist(token, playlist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={playlist_name}&type=playlist&limit=1"
-
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["playlists"]["items"]
-    if len(json_result) == 0:
-        print("No playlist found")
-        return None
-
-    return json_result[0]
-
-def playlist_songs(token, playlist_id):
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["items"]["TrackObject"]
-    return json_result
 
 token = get_token()
-result = find_playlist(token, "(head)Bangers")
+result = search_for_artist(token, "The Home Team")
 playlist_id = result["id"]
-print(playlist_id)
-songs = playlist_songs(token, playlist_id)
-print(songs)
 
-# artist_id = result["id"]
-# songs = get_songs(token, artist_id)
+artist_id = result["id"]
+songs = get_songs(token, artist_id)
 
-# for idx, song in enumerate(songs):
-    # print(f"{idx + 1}. {song['name']}")
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
