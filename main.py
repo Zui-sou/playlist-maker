035ad4433750f707ada2eb613af7d4af8af317b3
Zui-sou <edanrohner@gmail.com>
2025-02-24 20:56:21 -0700

    fixed indentation

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import json
 
-from requests import get, post
+from requests import get
 
 from auth import get_auth_header, get_token
 
 
 
-
 def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
+  url = "https://api.spotify.com/v1/search"
+  headers = get_auth_header(token)
+  query = f"?q={artist_name}&type=artist&limit=1"
 
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    if len(json_result) == 0:
-        print("No artist found")
-        return None
+  query_url = url + query
+  result = get(query_url, headers=headers)
+  json_result = json.loads(result.content)["artists"]["items"]
+  if len(json_result) == 0:
+    print("No artist found")
+    return None
 
-    return json_result[0]
+  return json_result[0]
 
 
 def get_songs(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
+  url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+  headers = get_auth_header(token)
+  result = get(url, headers=headers)
+  json_result = json.loads(result.content)["tracks"]
+  return json_result
 
 
 token = get_token()
@@ -38,4 +37,4 @@ artist_id = result["id"]
 songs = get_songs(token, artist_id)
 
 for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['name']}")
+  print(f"{idx + 1}. {song['name']}")
